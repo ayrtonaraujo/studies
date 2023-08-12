0e910800151b6fa20cc05fcaed2f607a383a0dfd
Ayrton Araújo <ayrtondenner@gmail.com>
2023-08-12 11:25:00 -0400

    12-08-2023

---
+++ c-lang/exercise-odd-numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Digite o primeiro numero inteiro: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo numero inteiro: ");
+    scanf("%d", &num2);
+
+    printf("Numeros impares entre %d e %d:\n", num1, num2);
+
+    // Certificar-se de que num1 é o menor número
+    if (num1 > num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    int current = num1;
+
+    while (current <= num2) {
+        if (current % 2 != 0) {
+            printf("%d\n", current);
+        }
+        current++;
+    }
+
+    return 0;
+}
