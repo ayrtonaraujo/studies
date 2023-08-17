4b5c1c4f95df686ee01ddf048fd9ef688a904936
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2023-08-17 15:52:22 -0400

    17-08-23

---
+++ c-lang/arrays-testing.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{   
+    int seg[11] = {0,1,2,3,4,5,6,7,8,9,10};
+    
+     
+    for (int i = 0 ; i<=10 ; i++){
+
+        printf ("\n%d", seg[i]);
+    }
+
+
+return 0;
+}
