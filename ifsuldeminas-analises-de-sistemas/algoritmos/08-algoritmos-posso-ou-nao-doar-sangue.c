9a22235a7c0810d8670fb90326fb2b974021dcc3
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2023-09-28 13:24:54 -0400

    28-09-23

---
+++ ifsuldeminas-analises-de-sistemas/algoritmos/08-algoritmos-posso-ou-nao-doar-sangue.c
@@ -0,0 +1,31 @@
+/*
+O sistema de doação de sangue deve seguir os seguintes critérios:
+    - Se a idade for menor que 16 anos ou maior que 70 anos não pode doar.
+    - Se a idade for entre 16 e 69 anos, pode doar, porém deve verificar se o peso do doador é o no mínimo 50kg.
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    int idade, peso;
+
+    printf ("Idade: ");
+    scanf ("%d", &idade);
+    getchar();
+
+    printf ("Peso: ");
+    scanf ("%d", &peso);
+    getchar();
+
+    if (idade >= 16 && idade <= 69 && peso >= 50) {
+        printf ("\nPode doar.");
+
+    }
+    else {
+        printf ("\nNão pode doar.");
+    }
+
+
+    return 0;
+}
---
+++ ifsuldeminas-analises-de-sistemas/algoritmos/exercicios-propostos/01-multiplo-de-5.c
@@ -0,0 +1,11 @@
+/*
+1. Implemente um pseucódigo que leia um número e informe se esse número é multipĺo de 5 ou não.
+*/
+
+#include <stdio.h>
+
+int main () {
+
+
+    return 0;
+}
