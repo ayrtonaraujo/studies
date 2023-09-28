db0f3d844e61770590044b2a3987c6db46aa5ecb
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2023-09-28 12:52:38 -0400

    28-09-23

---
+++ ifsuldeminas-analises-de-sistemas/06-raiz-quadrada-ou-quadrado-numero.c
@@ -0,0 +1,26 @@
+/*
+Entrar com um numero e raz quadrada caso ele seja positivo e quadrado do numero caso ele seja negativo.
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    float numero;
+
+    printf("Insira o valor: ");
+    scanf("%f", &numero);
+
+    if (numero >= 0)
+    {
+        printf("\nRaiz quadrada: %.2f", sqrt(numero));
+    }
+    else
+    {
+        printf("\nQuadrado do numero: %.2f", pow(numero, 2));
+    }
+
+    return 0;
+}
