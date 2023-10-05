fbfc1d2e41e9f09e1340884fc6273e55fb703bf7
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2023-10-05 07:32:32 -0400

    05-10-23

--- ifsuldeminas-analises-de-sistemas/algoritmos/exercicios-propostos/02-atividade/003-idade-pessoas-18-anos.c
+++ ifsuldeminas-analises-de-sistemas/algoritmos/exercicios-propostos/02-atividade/003-idade-pessoas-18-anos.c
@@ -4,9 +4,32 @@
 
 #include <stdio.h>
 
+#define NUMERO_DE_PESSOAS 5
+
 int main()
 {
-    
+    int idade[NUMERO_DE_PESSOAS];
+    int maiores_de_idade = 0;
+
+    for (int i = 1; i <= NUMERO_DE_PESSOAS; i++)
+    {
+        printf("Idade da Pessoa #%d: ", i);
+        scanf("%d", &idade[i - 1]);
+        getchar();
+
+        if (idade[i -1 ] < 0) {
+         printf ("Insira um valor valido. >= 0.\n");
+         i--;
+        continue;
+        }
+
+        if (idade[i - 1] >= 18)
+        {
+            maiores_de_idade++;
+        }
+    }
+
+    printf("\nQuantidade de maiores de idade: %d", maiores_de_idade);
 
     return 0;
 }
