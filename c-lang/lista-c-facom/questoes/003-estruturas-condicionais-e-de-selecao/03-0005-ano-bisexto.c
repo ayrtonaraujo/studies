fc2d3f00d3c2d970a983c61ca52881e3b414af52
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2024-01-02 20:32:14 -0400

    vergonha

--- c-lang/lista-c-facom/questoes/003-estruturas-condicionais-e-de-selecao/03-0005-ano-bisexto.c
+++ c-lang/lista-c-facom/questoes/003-estruturas-condicionais-e-de-selecao/03-0005-ano-bisexto.c
@@ -2,3 +2,19 @@
 5. Verificar se um determinado ano, fornecido pelo usuário, é bissexto. Um ano é bissexto se for divisível por 400 ou se for divisível por 4 e não for divisível por 100. Exemplos: 1988, 1992, 1996, 2000. Repare que 1900 não é bissexto.
 */
 
+#include <stdio.h>
+
+int main() {
+    int anoFornecido;
+
+    printf("Insira o ano: ");
+    scanf("%d", &anoFornecido);
+
+       if ((anoFornecido % 400 == 0) || ((anoFornecido % 4 == 0) && (anoFornecido % 100 != 0))) {
+        printf("O ano é bissexto!\n");
+    } else {
+        printf("O ano não é bissexto!\n");
+    }
+
+    return 0;
+}
