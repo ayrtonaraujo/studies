581779b52cc3088d4f8645e60215b08eef05688c
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2024-01-08 09:36:10 -0400

    Crescente

--- c-lang/lista-c-facom/questoes/003-estruturas-condicionais-e-de-selecao/03-00010-ordem_crescente.c
+++ c-lang/lista-c-facom/questoes/003-estruturas-condicionais-e-de-selecao/03-00010-ordem_crescente.c
@@ -4,15 +4,39 @@
 
 #include <stdio.h>
 
-int main ()
+int main()
 {
-    float numeros [3];
+    float numeros[3];
     int i;
 
-    for (i == 0; i <=3; i++) {
-        printf ("Insira o N%d: ", i+1);
-        scanf ("%f", numeros[])
+    for (i = 0; i < 3; i++)
+    {
+        printf("Valor de N%d: ", i + 1);
+        scanf("%f", &numeros[i]);
     }
+
+    if (numeros[0] > numeros[1])
+    {
+        float temp = numeros[0];
+        numeros[0] = numeros[1];
+        numeros[1] = temp;
+    }
+
+    if (numeros[1] > numeros[2])
+    {
+        float temp = numeros[1];
+        numeros[1] = numeros[2];
+        numeros[2] = temp;
+    }
+
+    if (numeros[0] > numeros[1])
+    {
+        float temp = numeros[0];
+        numeros[0] = numeros[1];
+        numeros[1] = temp;
+    }
+
+    printf("Ordem: %.2f, %.2f, %.2f", numeros[0], numeros[1], numeros[2]);
 
     return 0;
 }
