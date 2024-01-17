ed6bf1813b9cca3465905f6016fab3c114341617
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2024-01-17 13:01:37 -0400

    22

---
+++ c-lang/lista-c-facom/questoes/003-estruturas-condicionais-e-de-selecao/03-0022-notas_e_faltas.c
@@ -0,0 +1,116 @@
+/*
+22. Leia a nota e o número de faltas de um aluno e escreva seu conceito de acordo com a tabela 3.3.
+*/
+
+#include <stdio.h>
+
+void conceitoA();
+void conceitoB();
+void conceitoC();
+void conceitoD();
+void conceitoE();
+
+int main()
+{
+    float notaAluno;
+    int numeroFaltas;
+
+    printf("Nota final: ");
+    scanf("%f", &notaAluno);
+
+    printf("Numero de faltas: ");
+    scanf("%d", &numeroFaltas);
+
+    if (notaAluno >= 9.0 && notaAluno <= 10.0)
+    {
+        if (numeroFaltas <= 20)
+        {
+            conceitoA();
+        }
+
+        if (numeroFaltas > 20)
+        {
+            conceitoB();
+        }
+    }
+
+    if (notaAluno >= 7.5 && notaAluno <= 8.9)
+    {
+        if (numeroFaltas <= 20)
+        {
+            conceitoB();
+        }
+
+        if (numeroFaltas > 20)
+        {
+            conceitoC();
+        }
+    }
+
+    if (notaAluno >= 5.0 && notaAluno <= 7.4)
+    {
+        if (numeroFaltas <= 20)
+        {
+            conceitoC();
+        }
+
+        if (numeroFaltas > 20)
+        {
+            conceitoD();
+        }
+    }
+
+    if (notaAluno >= 4.0 && notaAluno <= 4.9)
+    {
+        if (numeroFaltas <= 20)
+        {
+            conceitoD();
+        }
+
+        if (numeroFaltas > 20)
+        {
+            conceitoE();
+        }
+    }
+
+    if (notaAluno >= 0.0 && notaAluno <= 3.9)
+    {
+        if (numeroFaltas <= 20)
+        {
+            conceitoE();
+        }
+
+        if (numeroFaltas > 20)
+        {
+            conceitoE();
+        }
+    }
+
+    return 0;
+}
+
+
+void conceitoA()
+{
+    printf("\nConceito A.");
+}
+
+void conceitoB()
+{
+    printf("\nConceito B.");
+}
+
+void conceitoC()
+{
+    printf("\nConceito C.");
+}
+
+void conceitoD()
+{
+    printf("\nConceito D.");
+}
+
+void conceitoE()
+{
+    printf("\nConceito E.");
+}
