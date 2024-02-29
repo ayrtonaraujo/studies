f85c6cd8108ea6dc9b0c0cfbf345af8c3d4485c5
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2024-02-29 09:12:14 -0400

    0027

---
+++ c-lang/lista-c-facom/questoes/003-estruturas-condicionais-e-de-selecao/03-0026-data_certa^.c
@@ -0,0 +1,4 @@
+/*
+26. Leia uma data e determine se a data fornecida é válida. Lembre-se que o mês de fevereiro tem 29 dias em anos bissextos.
+*/
+
---
+++ c-lang/lista-c-facom/questoes/003-estruturas-condicionais-e-de-selecao/03-0027_acrescimo_de_valores.c
@@ -0,0 +1,55 @@
+/*
+27. Um certo produto irá sofrer aumento de acordo com a tabela 3.4. Faça um programa que leia o valor atual, calcule e escreva o novo valor do produto. O programa também deve mostrar uma mensagem de acordo com a tabela 3.5.
+*/
+
+
+#include <stdio.h>
+
+#define REAJUSTE_5 0.05
+#define REAJUSTE_10 0.10
+#define REAJUSTE_15 0.15
+
+int main ()
+{
+    float valor_produto, novo_valor, acrescimo;
+
+    printf("Insira o valor do produto: ");
+    scanf ("%f", &valor_produto);
+
+    if (valor_produto <= 50.0) {
+        novo_valor = valor_produto + (valor_produto * REAJUSTE_5);
+        acrescimo = valor_produto * REAJUSTE_5;
+    }
+
+    if (valor_produto >= 50.0 && valor_produto <= 100) {
+        novo_valor = valor_produto + (valor_produto * REAJUSTE_10);
+        acrescimo = valor_produto * REAJUSTE_10;
+    }
+
+    if (valor_produto > 100.0) {
+        novo_valor = valor_produto + (valor_produto * REAJUSTE_15);
+        acrescimo = valor_produto * REAJUSTE_15;
+    }
+    
+    printf ("Valor com acrescimo: %.2f", novo_valor);
+    printf ("\nValor do acrescimo: %.2f", acrescimo);
+    
+    
+    if (valor_produto <= 80.0) {
+        printf ("\nBarato.");
+    }
+
+    if (valor_produto >= 80.0 && valor_produto <= 120) {
+        printf ("\nNormal.");
+    }
+
+    if (valor_produto >= 120 && valor_produto <= 200) {
+        printf ("\nCaro.");
+    }
+
+    if (valor_produto > 200) {
+        printf ("\nMuito caro.");
+    }
+
+    return 0;
+}
