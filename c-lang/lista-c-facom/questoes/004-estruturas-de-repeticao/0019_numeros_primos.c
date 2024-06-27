019abaf803161bd7913984800a428234e6ff34c7
Ayrton Araújo <95201381+ayrtonaraujo@users.noreply.github.com>
2024-06-27 10:16:30 -0400

    Create 0019_numeros_primos.c

---
+++ c-lang/lista-c-facom/questoes/004-estruturas-de-repeticao/0019_numeros_primos.c
@@ -0,0 +1,26 @@
+/*
+Faça um programa que receba um número maior que 1, e verifique se o número fornecido é primo ou não.
+Obs.: um número é considerado primo quando seus únicos divisores são 1 e o proprio número.
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    int numero_usuario = 1, primo;
+
+    printf("Digite um numero maior que 1.");
+
+    while (numero_usuario > 1)
+    {
+        printf("\nDigite um numero: ");
+        scanf("%d", &numero_usuario);
+
+        primo = numero_usuario % numero_usuario;
+        primo = 
+        
+    }
+    
+
+    return 0;
+}
