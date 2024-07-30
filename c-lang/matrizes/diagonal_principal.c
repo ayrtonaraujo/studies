55773999537c760611fa855070cdcca7d852f841
Ayrton Araujo <ayrtondenner@gmail.com>
2024-07-30 17:02:19 -0400

    Diagona da matriz, exercício.

--- c-lang/matrizes/diagonal_principal.c
+++ c-lang/matrizes/diagonal_principal.c
@@ -1,3 +1,8 @@
+/*
+Fazer um programa para ler um número inteiro N (máximo = 10) e uma matriz quadrada de ordem N contendo números inteiros. Em seguida, mostrar a diagonal principal e a quantidade de valores negativos da matriz.
+*/
+
+
 #include <stdio.h>
 
 int main()
