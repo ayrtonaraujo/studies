381d9e8237d328f55ff0624875fac417f81a7b7a
Ayrton Araujo <ayrtondenner@gmail.com>
2024-09-02 00:33:31 -0400

    Bora, bora

--- c-lang/lista-c-facom/questoes/004-estruturas-de-repeticao/0033_terno_pitagorico.c
+++ c-lang/lista-c-facom/questoes/004-estruturas-de-repeticao/0033_terno_pitagorico.c
@@ -1,17 +1,28 @@
 /*
-Faça um programa que calcule o terno pitagórico a, b, c, para o qual a + b + c = 1000. Um terno pitagórico é um conjunto de três números naturais, a b c, para a qual, a² + b² = c²
-Por exemplo, 3² + 4² = 9 + 16 = 25 = 5²
+Faça um programa que calcule o terno pitagórico a, b, c, para o qual a + b + c = 1000. Um terno pitagórico é um conjunto de três números naturais, a b c, para a qual, a² + b² = c². Por exemplo, 3² + 4² = 9 + 16 = 25 = 5²
 */
 
 #include <stdio.h>
+#include <math.h>
 
-int main (void)
+int main(void)
 {
-    int a, b, c;
+    int a, b, c, soma;
 
-    for ()
+    for (int i = 1; i < 1000; i++)
     {
-        
+        for (int j = i + 1; j < 1000; j++)
+        {
+            a = i;
+            b = j;
+            c = sqrt(pow(a, 2) + pow(b, 2));
+            soma = a + b + c;
+
+            if (soma == 1000)
+            {
+                printf("\nO terno pitagórico eh:\nA = %d\nB = %d\nC = %d", a, b, c);
+                return 0;
+            }
+        }
     }
-    
 }
