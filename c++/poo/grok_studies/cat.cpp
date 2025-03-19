73494f8f255bcf40f27dd634a8955fef31799361
Ayrton Araujo <ayrtondenner@gmail.com>
2025-03-19 17:39:57 -0400

    OOP Studys

---
+++ c++/poo/grok_studies/cat.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Cat {
+    private:
+        string color;
+    public:
+        Cat(string c){
+            color = c;
+        }
+
+        void meow(){
+            cout << color << " cat says Meow!\n";
+        }
+};
+
+int main (){
+
+    Cat gato("Orange");
+    gato.meow();
+
+    return 0;
+}
