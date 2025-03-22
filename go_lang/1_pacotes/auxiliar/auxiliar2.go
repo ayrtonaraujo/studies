46236ccf607f978fa50a157c28d7265b27105a39
Ayrton Araújo <ayrtondenner@gmail.com>
2025-03-21 21:40:56 -0400

    Starting studies in go_lang

---
+++ go_lang/1_pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// escreve "auxiliar 2" na tela
+func Escrever2() {
+	fmt.Println("Auxiliar 2")
+}
---
+++ go_lang/1_pacotes/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+func main() {
+	fmt.Println("Main")
+	auxiliar.Escrever()
+	auxiliar.Escrever2()
+}
