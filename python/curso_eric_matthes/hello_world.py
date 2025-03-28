3a2b185964e5ac99b320289eacf57fa521304c6d
Ayrton Araújo <ayrtondenner@gmail.com>
2025-03-27 22:51:37 -0400

    Estudos do final da noite

---
+++ python/curso_eric_matthes/hello_world.py
@@ -0,0 +1,9 @@
+msg = ['Hello, world!', 'ada lovelace', 'alan turing', 'ALBERT EINSTEIN']
+
+print(msg[0])
+print(msg[1].title())
+print(msg[2].upper())
+print(msg[3].lower())
+
+print(msg[0].lstrip())
+
