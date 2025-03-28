e26b35b49379c70d28970fafe7c4356deed54cfa
Ayrton Araújo <ayrtondenner@gmail.com>
2025-03-28 12:23:37 -0400

    Criação da pasta cs50

---
+++ cs50/c_lang/meow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+
+    int count = 1;
+
+    while (count <= 3) {
+
+        printf ("Meow!\n");
+        count++;
+    }
+
+    return 0;
+}
---
+++ cs50/c_lang/strings.c
@@ -0,0 +1,13 @@
+//lecture 2
+
+#include <stdio.h>
+
+int main (void)
+{
+    char string_test[] = "Abaco";
+    
+
+    printf ("%s", string_test);
+
+    
+}
